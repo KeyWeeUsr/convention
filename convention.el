c63cee90cd5196adfa440070d6309a3116bbb554
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2025-06-15 23:15:27 +0200

    feat(init): Add package skeleton

---
+++ convention.el
@@ -0,0 +1,31 @@
+;;; convention.el --- Enable conventional syntax -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025 Peter Badida
+
+;; Author: Peter Badida <keyweeusr@gmail.com>
+;; Keywords: convenience, conventional, mode, helper, git, comment, commit
+;; Version: 1.0.0
+;; Package-Requires: ((emacs "28.1"))
+;; Homepage: https://github.com/KeyWeeUsr/convention
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; A collection of minor modes supporting conventional syntax.
+
+;;; Code:
+
+(provide 'convention)
+;;; convention.el ends here
